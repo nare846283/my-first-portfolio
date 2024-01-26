7d588c9a0d3ac835cef71a2f937a85dc21fac2b3
Narendra Ahirwar <71281553+nare846283@users.noreply.github.com>
2024-01-26 21:52:20 +0530

    Update script.js

--- script.js
+++ script.js
@@ -62,3 +62,74 @@ function sendEmail() {
   // Open default email client to send the email
   window.location.href = mailtoLink;
 }
+
+function projectsData() {
+  const projects = [
+    {
+      name: "Sundown Web Page",
+      technologies: "HTML / CSS / JS / SwipperJs",
+      videoUrl: "images/sundown.mp4",
+      demoUrl: "https://nare846283.github.io/sundown-web-home-page/",
+    },
+    {
+      name: "Netflix Clone Web",
+      technologies: "HTML / CSS / JS",
+      videoUrl: "images/Netflix Web .mp4",
+      demoUrl: "https://nare846283.github.io/netflix-clone-using-html-css-jss/",
+    },
+    {
+      name: "Amazone Web Clone",
+      technologies: "HTML / CSS / JS / SwipperJs",
+      videoUrl: "images/amazone clone home page.mp4",
+      demoUrl: "https://nare846283.github.io/amazone-clone-using-html-css-jaa/",
+    },
+    {
+      name: "Start Globle Website",
+      technologies: "HTML / CSS",
+      videoUrl: "images/StartGlobal Web.mp4",
+      demoUrl:
+        "https://nare846283.github.io/startgloble-web-clone-complete-home-page-created-using-html-css/",
+    },
+    {
+      name: "Vim & Vigour",
+      technologies: "HTML / CSS / JS",
+      videoUrl: "images/vim and vigour.mp4",
+      demoUrl:
+        "https://nare846283.github.io/Vim-Vigour-web-clone-complete-home-page-design-using-html-css/",
+    },
+
+    {
+      name: "Another Web",
+      technologies: "HTML / CSS",
+      videoUrl: "images/another.mp4",
+      demoUrl: "https://nare846283.github.io/sundown-web-home-page/",
+    },
+    // Add other projects similarly
+  ];
+
+  // Get the projects container element
+  const projectsContainer = document.getElementById("projects-container");
+
+  // Loop through the projects and create HTML for each
+  projects.forEach((project) => {
+    const projectElement = document.createElement("div");
+    projectElement.classList.add("project");
+
+    projectElement.innerHTML = `
+  
+        <div class="project-text">
+            <h2 class="project-name">${project.name}</h2>
+            <h4 class="project-technologies">${project.technologies}</h4>
+        </div>
+        <video autoplay muted loop>
+              <source src="${project.videoUrl}" type="video/mp4">
+        </video>
+        <a href="${project.demoUrl}" class="project-link" target="_blank">View Demo</a>
+    
+    `;
+
+    projectsContainer.appendChild(projectElement);
+  });
+}
+
+projectsData();
